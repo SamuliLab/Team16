8ee5446710858fab1f6b617ee7249d4131b79e29
Maria Hiekkamäki <maria.hiekkamaki@gmail.com>
2025-12-08 11:20:15 +0200

    added endMsg
    added TextEndMsg

--- 2D Platformer/Assets/Scripts/MainController.cs
+++ 2D Platformer/Assets/Scripts/MainController.cs
@@ -1,49 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class MainController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-
-
+public class MainController : MonoBehaviour
 {
-    public Texture2D hoverCursor;
-    private Texture2D defaultCursor;
-    public Vector2 hotSpot = Vector2.zero;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        defaultCursor = null;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-public void StartLevel(int levelIndex)
+    // Start button calls this
+    public void StartLevel(int levelIndex)
     {
         SceneManager.LoadScene(levelIndex);
-
     }
 
-public void QuitGame()
+    // Quit button calls this
+    public void QuitGame()
     {
         Application.Quit();
     }
-public void OnPointerEnter(PointerEventData eventData)
-    {
-        if (eventData.pointerEnter != null && eventData.pointerEnter.GetComponent<Button>())
-        {
-            Cursor.SetCursor(hoverCursor, hotSpot, CursorMode.Auto);
-        }
-    }
-
-public void OnPointerExit(PointerEventData eventData)
-    {
-        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
-    }
 }
+
 
 
